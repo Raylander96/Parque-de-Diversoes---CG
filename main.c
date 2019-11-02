dbde4fc99dbce8c8d22533cb2b1cc9ca6c34aaa2
Raylander <raylanderlopes@hotmail.com>
2019-11-02 18:59:34 -0300

    teste import obj

--- main.c
+++ main.c
@@ -13,6 +13,10 @@
 #include "libs/ferris_wheel.h"
 #include "libs/song.h"
 
+GLuint elephant;
+float elephantrot;
+char ch='1';
+
 void display();
 
 
@@ -41,12 +45,14 @@ void display(){
 			glTranslatef(co_x, co_y, -co_z);
 		glPopMatrix();
 	glPushMatrix();
-	
 	glTranslatef(gw_x, gw_y, -gw_z);
+	drawElephant();
 	glRotatef(gw_spin, 0.0, 0.0, 1.0);
+	
 	draw_gwheel();
 	glPopMatrix();
-	
+	//glLoadIdentity();
+    
 	
 	glutSwapBuffers();
 
@@ -81,7 +87,8 @@ void fogEnable(){
 	
 }
 void keyboard(unsigned char key, int x, int y){
-
+	if(key=='+') movcord[1]--;
+	if(key=='-') movcord[1]++;
 	if(key=='w'  || key == 'W')
 	{
 		movcord[0]+=5*cos(-1*x_r*3.14/180.0);
@@ -112,6 +119,53 @@ void keyboard(unsigned char key, int x, int y){
 	display();
 }
 
+void loadObj(char *fname)
+{
+    FILE *fp;
+    int read;
+    GLfloat x, y, z;
+    char ch;
+    elephant=glGenLists(1);
+    fp=fopen(fname,"r");
+    if (!fp)
+    {
+        printf("can't open file %s\n", fname);
+        exit(1);
+    }
+    glPointSize(5.0);
+    glNewList(elephant, GL_COMPILE);
+    {
+        glPushMatrix();
+        glBegin(GL_POINTS);
+        while(!(feof(fp)))
+        {
+            read=fscanf(fp,"%c %f %f %f",&ch,&x,&y,&z);
+            if(read==4&&ch=='v')
+            {
+                glVertex3f(x,y,z);
+            }
+        }
+        glEnd();
+    }
+    glPopMatrix();
+    glEndList();
+    fclose(fp);
+}
+
+void drawElephant()
+{
+    loadObj("images/al.obj");
+    glPushMatrix();
+    glTranslatef(0,-40.00,-105);
+    glColor3f(1.0,0.23,0.27);
+    glScalef(20.9,20.9,20.9);
+    glRotatef(elephantrot,0,1,0);
+    glCallList(elephant);
+    glPopMatrix();
+    elephantrot=elephantrot+0.6;
+    if(elephantrot>360)elephantrot=elephantrot-360;
+}
+
 int main(int argc, char** argv){
 
    		SDL_Init (SDL_INIT_AUDIO);
@@ -120,7 +174,7 @@ int main(int argc, char** argv){
 
 		glutInit(&argc,argv);
 		glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
-		glutInitWindowSize(800,600);
+		glutInitWindowSize(1500,1200);
 		glutCreateWindow("Parque de Diversao");
 		initLights();
 		initSky();
